8bc5a13b28af0a3644efc72869eb3f7f984bdd55
jinseok-kr <tjrl1225@gmail.com>
2023-09-21 22:11:08 +0900

    Init multi-module

--- server/src/main/java/com/multi/server/agent/repository/AgentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.multi.server.agent.repository;
-
-import com.multi.server.agent.entity.Agent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AgentRepository extends JpaRepository<Agent, String> {
-}
--- server/src/main/java/com/multi/server/agent/service/AgentService.java
+++
@@ -1,9 +0,0 @@
-package com.multi.server.agent.service;
-
-import com.multi.server.agent.dto.RegistAgentRequestDTO;
-
-import java.util.List;
-
-public interface AgentService {
-    public void registAgent(RegistAgentRequestDTO registAgentRequestDTO);
-}
